919a0221b7aa8b81f38a4acc5254eaf085ebb148
Daniick56 <danielballesteros22@outlook.com>
2024-05-14 06:19:33 -0500

    guardando info.

---
+++ Iteration-While/src/ejercicio2.java
@@ -0,0 +1,9 @@
+/**/
+
+import java.util.Scanner;
+
+public class ejercicio2 {
+    public static void main(String[] args){
+
+    }
+}
---
+++ Iteration-While/src/ejercicio3.java
@@ -0,0 +1,9 @@
+/**/
+
+import java.util.Scanner;
+
+public class ejercicio3 {
+    public static void main(String[] args){
+
+    }
+}
